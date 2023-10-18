444559576e1b7979f1be56576fc23d4a27d04689
Student <ceontewilliams820@gmail.com>
2023-10-18 14:22:22 -0400

    all saved items

---
+++ NumericConversions/conversion_test.js
@@ -0,0 +1,30 @@
+"use strict"
+
+//description: This script tests various numeric
+//             conversion techniques
+// Author: Ceonte W. NewProgrammer
+
+var a = parseInt("101.1");   
+var b = parseInt("55");
+var c = parseInt("402 stevens");
+var d = parseInt("Number 5");
+
+var a = parseFloat("101.1");
+var b = parseFloat("55");
+var c = parseFloat("402 stevens");
+var d = parseFloat("Number 5");
+
+
+var a = Number("101.1");
+var b = Number("55");
+var c = Number("402 stevens");
+var d = Number("Number 5");
+
+a - +"101.1";
+b - +"55";
+c - +"402 stevens";
+d - +"5";
+
+
+
+
